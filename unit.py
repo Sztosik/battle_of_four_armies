0db50a8a01fdc7385c1817603d1b5187bc858169
kykyCode <68389981+kykyCode@users.noreply.github.com>
2021-04-26 12:59:53 +0200

    Project 0.0.4

--- unit.py
+++ unit.py
@@ -1,13 +1,13 @@
 import random
 import config
 
-class Base_unit:
+class Unit:
     def __init__(self, id, fraction, pos_x, pos_y):
         self.__id = id
         self.__fraction = fraction
         self.__health_points = config.BASE_UNIT_HP
         self.__strength = config.BASE_UNIT_STRENGTH
-        self.__movement_points = config.BASE_UNIT_MOVMENT_POINT
+        self.__movement_points = config.BASE_UNIT_MOVEMENT_POINT
         self.__pos_x = pos_x
         self.__pos_y = pos_y
         self.__is_alive = True
@@ -16,51 +16,89 @@ class Base_unit:
         if self.__is_alive == True:
             movement = self.__movement_points
             while movement > 0:
-                rand_x = random.randint(-1, 1)
-                rand_y = random.randint(-1, 1)
+                rand_x = random.randomint(-1, 1)
+                rand_y = random.randomint(-1, 1)
                 pos_x = self.__pos_x + rand_x
                 pos_y = self.__pos_y + rand_y
 
-                # zwraca 0: jeśli pole nie istnieje,  1: jeśli na polu nie ma przeciwnika,  2: na polu znajduje się przeciwnik
                 move = board.is_it_free(pos_x, pos_y, self.__fraction)
                 if move == 1:
-                    # usuwa jednostkę z listy jednostek pola na którym stała wcześniej, przenieść to do funkcji captude_the_field
                     board.board_fields[pos_y][pos_x].remove_unit(self.__id)
-                    # aktualizuje pozycje jednostki
                     self.__pos_x = pos_x
                     self.__pos_y = pos_y
                     self.capture_the_field(board, pos_x, pos_y)
                     movement -= 1
                 if move == 2:
                     self.fight()
-                    movement -= 1
-    
+                    movement -=1
+
+
     def capture_the_field(self, board, x, y):
-        # zmienia przynależność pola
-        board.board_fields[y][x].change_fraction(self.__fraction)
-        # dodaje jednostkę do listy jednostek znajdujących się na tym polu
-        board.board_fields[y][x].add_unit(self.__id)
+            board.board_fields[y][x].change_fraction(self.__fraction)
+            board.board_fields[y][x].add_unit(self.__id)
 
     def fight(self, board, x, y):
-        # id pierwszego przeciwnika znajdującego się na polu
         enemy_id = board.board_fields[y][x].get_units()[0]
         enemy_fraction = board.board_fields[y][x].get_fraction()
-        # niech pole przechowuje nie id ale ardesy obiektów (jednostek) w pamięci i będzie git
 
-    def get_demage(self):
-        pass
+class Base_Unit(Unit):
+    def __init__(self, id, fraction, pos_x, pos_y):
+        super().__init__(id, fraction, pos_x, pos_y)
 
-    def die(self):
-        self.__is_alive = False
 
-class Unit_A:
-    pass
 
-class Unit_B:
-    pass
+#silniejsza i bardziej wytrzymala, walczy z kilkoma jednostkami
+class Special_Unit_A(Unit):
+    def __init__(self, id, fraction, pos_x, pos_y):
+        super().__init__(id, fraction, pos_x, pos_y)
+        self.__health_points = config.SPECIAL_UNIT_HP
+        self.__strength = config.SPECIAL_UNIT_STRENGTH
 
-class Unit_C:
-    pass
+#wiecej movement pointsow
+class Special_Unit_B(Unit):
+    def __init__(self, id, fraction, pos_x, pos_y):
+        super().__init__(id, fraction, pos_x, pos_y)
+        self.__movement_points = config.SPECIAL_UNIT_MOVEMENT_POINT
 
-class Unit_D:
-    pass
+#przejmowanie terernu z dystansu
+class Special_Unit_C(Unit):
+
+    def takeover_from_a_distance(self, board, x ,y):
+        throw_direction = random.randint(0, 3) # 0:góra   1:prawo   2:dół   3:lewo
+        throw_distance = random.randint(2, 10)
+
+        if throw_direction == 0:
+            target_pos_x = x
+            target_pos_y = y - throw_distance
+
+        elif throw_direction == 1:
+            target_pos_x = x + throw_distance
+            target_pos_y = y
+
+        elif throw_direction == 2:
+            target_pos_x = x
+            target_pos_y = y + throw_distance
+
+        elif throw_direction == 3:
+            target_pos_x = x - throw_distance
+            target_pos_y = y
+
+        for value in range(3):
+            for value2 in range(3):
+                if board.is_it_free(target_pos_y-1+value,target_pos_x-1+value2, self.__fraction) == 1:
+                    board.board_fields[target_pos_y-1+value][target_pos_x-1+value2].change_fraction(self.__fraction)
+
+#przejmuje pola sasiednie
+class Special_Unit_D(Unit):
+
+    #przejmuje wszystkie pola z ktorymi sasiaduje i są wolne : np x=2, y=2 przejmuje:
+    # (1,1)(2,1)(3,1)
+    # (1,2)[2,2](3,2)
+    # (1,3)(2,3)(3,3)
+
+    def capture_the_field(self, board, x, y):
+            board.board_fields[y][x].add_unit(self.__id)
+            for value in range(3):
+                for value2 in range(3):
+                    if board.is_it_free(y-1+value, x-1+value2, self.__fraction) == 1:
+                        board.board_fields[y-1+value][x-1+value2].change_fraction(self.__fraction)
