34a24a293cd055a0f6c9b69e5d2429270e82c986
kykyCode <68389981+kykyCode@users.noreply.github.com>
2021-05-31 13:39:21 +0200

    commit 744

--- simulation.py
+++ simulation.py
@@ -1,6 +1,9 @@
 import config
 from army import Army
 from board import Board
+from jsonization import jsonization
+import json
+from visualization import main_visualization
 
 
 class Simulation:
@@ -11,29 +14,39 @@ class Simulation:
         self.__board_y = 100
         self.board = Board(self.__board_x, self.__board_y)
 
-        new_army = Army("Lemon", 3, 3, 1, 1, self.board)
+        new_army = Army("Lemon", 15, 7, 1, 1, self.board)
         self.__armies.append(new_army)
-        new_army = Army("Green", 3, 3, 1, 99, self.board)
+        new_army = Army("Green", 15, 7, 1, 99, self.board)
         self.__armies.append(new_army)
-        new_army = Army("Blue", 3, 3, 99, 1, self.board)
+        new_army = Army("Blue", 15, 7, 99, 1, self.board)
         self.__armies.append(new_army)
-        new_army = Army("Transparent", 3, 3, 99, 99, self.board)
+        new_army = Army("Transparent", 15, 7, 99, 99, self.board)
         self.__armies.append(new_army)
 
     def start(self):
         """
         Rozpoczyna symulacje.
         """
-        for i in range(0, 5):
+        data = []
+        single_itr_data = []
+        for i in range(0, 500):
             # oczywiście trzeba zmienić ten warunek, dodać info o zajętych polach i jednostkach do klasy armia
             print("\n\niteration number = %s" % i)
             for army in self.__armies:
                 # print("\nARMY %s TURN" %army.fraction)
                 army.start(self.board)
             # self.save_stats()
+            single_itr_data.append(self.board.get_all_fields_data())
+            data.append(jsonization(self.board, self.__board_x, self.__board_y))
         self.board.captured_fields()
 
-    def save_stats():
+        with open("sample.json", "w") as outfile:
+            json.dump(data, outfile)
+
+        path = 'sample.json'
+        main_visualization(path)
+
+    def save_stats(self):
         pass
 
 
