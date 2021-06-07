4b4401c6ef41fb0f7edc5fe6791602aa99b6f8d7
kykyCode <68389981+kykyCode@users.noreply.github.com>
2021-06-07 14:06:56 +0200

    responsive screen size

--- battle/simulation/simulation.py
+++ battle/simulation/simulation.py
@@ -49,7 +49,7 @@ class Simulation:
             json.dump(data, outfile)
 
         path = "sample.json"
-        main_visualization(path)
+        main_visualization(path, self.__board_x, self.__board_y)
 
     def save_stats(self):
         pass
