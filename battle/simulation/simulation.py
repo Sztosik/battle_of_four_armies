2cc79b230a8ad512409b92af249fe74068d7b780
Jędrzej Stasik <sjedrzej58@gmail.com>
2021-06-08 13:13:42 +0200

    ADD: captured fields stats

--- battle/simulation/simulation.py
+++ battle/simulation/simulation.py
@@ -8,6 +8,7 @@ from queue import Queue
 import battle.simulation.config as config
 from battle.army.army import Army
 from battle.board.board import Board
+from battle.simulation.stats import Stast
 from battle.visualization.visualization import main_visualization
 
 logging.basicConfig(level=config.LOGGING_LEVEL)
@@ -24,6 +25,7 @@ class Simulation:
         self.__board_y: int = 50
         self.board: Board = Board(self.__board_x, self.__board_y)
         self.__end_condition = 0.5
+        self.stats = Stast()
 
         new_army = Army("Red", 0, 80, config.POS_RED, self.board)
         self.__armies.append(new_army)
@@ -50,13 +52,16 @@ class Simulation:
             time.sleep(config.SINGLE_FRAME_DURATION / 1000)
 
             iteration_stats = self.board.captured_fields()
+            self.stats.add_row(iteration_stats)
             maximum = max(iteration_stats.values())
             percentage_of_captured_fields = maximum / (self.__board_x * self.__board_y)
             logger.critical(iteration_stats)
+            # logger.critical(iteration_stats.keys())
             
             if percentage_of_captured_fields > self.__end_condition:
                 winner = max(iteration_stats, key=iteration_stats.get)
                 logger.critical(f"ITERATION: {iteration} WINNER: {winner}")
+                self.stats.save_to_csv()
                 break
         sys.exit()
 
@@ -78,8 +83,6 @@ class Simulation:
         simulation_thread.join()
         visualization_thread.join()
 
-    def save_stats(self):
-        pass
 
 
 if __name__ == "__main__":
--- battle/simulation/stats.py
+++ battle/simulation/stats.py
@@ -1,11 +1,29 @@
-class Stast:
-    def __init__(self, BaseUnits: int, special_units: int, fiealds: int):
-        self.BaseUnits = BaseUnits
-        self.special_units = special_units
-        self.fiealds = fiealds
+import csv
 
-    def save_to_json(self):
-        pass
+class Stast:
+    def __init__(self):
+        self.__units_stats: list[dict] = []
+        self.__captured_fields_stats: list[dict] = []
+
+    def add_row(self, captured_fields_row: dict):
+        # self.__units_stats.append(units_row)
+        self.__captured_fields_stats.append(captured_fields_row)
+
+    def get_rows(self, stats_list: list[dict]):
+        for stats_dict in stats_list:
+            yield stats_dict
+
+    def get_header(self, stats_list: list[dict]):
+        stats_dict = stats_list[0]
+        header = stats_dict.keys()
+        return header
 
     def save_to_csv(self):
-        pass
+        with open('captured_fields_stats.csv', 'w') as file:
+            fieldnames = self.get_header(self.__captured_fields_stats)
+            csv_writer = csv.DictWriter(file, delimiter=',', fieldnames=fieldnames)
+            csv_writer.writeheader()
+            for row in self.get_rows(self.__captured_fields_stats):
+                csv_writer.writerow(row)
+                # print(row)
+        print("Stats saved into captured_fields_stats.csv")
